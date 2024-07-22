c20cb005ff5e9c7b50ee6865fc53ae7a8169915f
Nicola Getsee <Nicola.getsee@gmail.com>
2024-07-22 19:53:26 +0000

    requirements.txt, ProcFile and settings.py with debug set to false and heroku '.herokuapp.com' added to allowed host

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=%o1h(rv#^%mny6y-4vxv2x#7)yuvybxjs+=_(d&w+md#mywbg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['8000-nikkig087-djangoproject-l8oyqvurbnm.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = ['8000-nikkig087-djangoproject-l8oyqvurbnm.ws.codeinstitute-ide.net','.herokuapp.com']
 
 
 # Application definition
